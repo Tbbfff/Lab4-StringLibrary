07c9a5fad2752e48aad9744f1d37cc6dc1fc2790
tbbfff <tbrunobf@gmail.com>
2026-04-17 13:47:20 -0400

    Add ShowCase app

---
+++ ShowCase/Program.cs
@@ -0,0 +1,8 @@
+﻿using StringLibrary;
+
+string[] words = ["Hello", "world", "Canada", "dotnet", "Azure"];
+
+foreach (string word in words)
+{
+    Console.WriteLine($"{word} starts with uppercase: {word.StartsWithUpper()}");
+}
